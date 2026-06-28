d19cc7732b845f68b432507bcb73ebd666a8950a
z00m0fficial <cras.mbell@gmail.com>
2026-06-28 14:21:26 -0700

    feat: add provider engine exports

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export type { ExecutionResult, ProviderAdapter, ProviderExecutedEvent, TaskEnvelope } from "./types.js";
+export { ProviderEngine } from "./ProviderEngine.js";
+export { ProviderRegistry } from "./ProviderRegistry.js";
+export { verifyExecution } from "./VerificationEngine.js";
+export { MockAdapter } from "./adapters/MockAdapter.js";
+export { CodexAdapter } from "./adapters/CodexAdapter.js";
